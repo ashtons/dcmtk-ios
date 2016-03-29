1cdb79cf4fc19d73b3e256792161d5d87df2ca08
Sean Ashton <sean.ashton@schimera.com>
2016-03-29 14:45:16 +1000

    Remove redundant DCMTK folder

--- dcmtk/doxygen/man2text.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Generate a text representation of the man pages
-cd manpages/man1/
-for file in *.1 ; do \
-	man -l $file | col -bxp > ../../man2text/`echo $file | sed 's/\(.*\.\)1/\1txt/'` ;\
-done
