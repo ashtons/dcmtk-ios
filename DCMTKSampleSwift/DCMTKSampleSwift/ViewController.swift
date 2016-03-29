425c489e6756993f03ceacaca2228e94fd14f873
Sean Ashton <sean.ashton@schimera.com>
2016-03-29 14:51:48 +1000

    Adding new Swift example project

---
+++ DCMTKSampleSwift/DCMTKSampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DCMTKSampleSwift
+//
+//  Created by Sean Ashton on 29/03/2016.
+//  Copyright © 2016 Schimera Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
